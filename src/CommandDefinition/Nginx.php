0fb4431aa8c08f0b276d82fae26ac4b163133da9
Li <welcome@watchowl.io>
2017-10-26 20:17:51 +0800

    done MySql command

---
+++ src/CommandDefinition/Nginx.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xu
+ * Date: 26/10/17
+ * Time: 8:15 PM
+ */
+
+namespace WatchOwl\CakeServerMonitor\CommandDefinition;
+
+
+class Nginx
+{
+
+}
---
+++ tests/CommandDefinition/NginxTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xu
+ * Date: 26/10/17
+ * Time: 8:16 PM
+ */
+
+namespace WatchOwl\CakeServerMonitor\Test\CommandDefinition;
+
+use WatchOwl\CakeServerMonitor\CommandDefinition\Nginx;
+
+class NginxTest extends \PHPUnit_Framework_TestCase
+{
+
+}
