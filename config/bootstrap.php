01f43552b47463ca919ed587b3ce2af443eafdda
Li <welcome@watchowl.io>
2017-10-30 06:43:53 +0800

    add all monitors

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,10 +2,18 @@
 
 use \Cake\Core\Configure;
 use WatchOwl\CakeServerMonitor\CommandDefinition\DiskSpace;
+use WatchOwl\CakeServerMonitor\CommandDefinition\MySql;
+use WatchOwl\CakeServerMonitor\CommandDefinition\Nginx;
+use WatchOwl\CakeServerMonitor\CommandDefinition\Php5Fpm;
 
 Configure::write(
     'CakeServerMonitor.commands',
-    ['disk_space' => DiskSpace::class]
+    [
+        'disk_space' => DiskSpace::class,
+        'mysql' => MySql::class,
+        'nginx' => Nginx::class,
+        'php5fpm' => Php5Fpm::class,
+    ]
 );
 
 Configure::write(
